70d9899549c0fa245bd93f900a68b552276d2d0e
Brodbeatz <144702428+Brodbeatz@users.noreply.github.com>
2026-03-10 12:42:23 -0500

    Got output redir working

--- core.cpp
+++ core.cpp
@@ -20,6 +20,7 @@ Needs to be able to store commands into a file(history).
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <fcntl.h>
+#include <fstream>
 
 using namespace std;
 
@@ -71,7 +72,7 @@ int stdInRead(vector<string> line){
     return index;
 }
 
-int hasPipe(vector<string> line){
+int stdHasPipe(vector<string> line){
     int index = -1;
     for (int i = 0; i < line.size(); ++i) {
         if (line[i] == "|") {
@@ -90,14 +91,15 @@ int main(){
     //storage file
     string historyLog = "hist_log.txt";
     //get current working directory
-    string directory = getcwd();
+    char cwd[1024];
+    string directory = getcwd(cwd, sizeof(cwd));
     //user input
     string in;
     //bool for exiting
     bool ender = false;
     //prep-pipes
-    toP[2];
-    toC[2];
+    int toP[2];
+    int toC[2];
     if(pipe(toP) < 0){
         cout << "ERROR: Cannot create pipe (toP)" << endl;
         exit(1);
@@ -114,17 +116,18 @@ int main(){
 
 	//start up the "terminal"
     while(!ender){
-        char* cmdLine;
         cout << directory << "~$";
-        getline(in, line);
+        string line;
+        getline(cin, line);
         addToHistory(line);
-        if(line = "exit"){
+        vector<string> command = tokenize(line);
+        if(line == "exit"){
             ender = true;
         }
-        else if(line = "history"){
+        else if(line == "history"){
             //print the history
             ifstream file(historyLog);
-            if(!file.is_open){
+            if(!file.is_open()){
                 cout << "ERROR: File not found" << endl;
                 exit(3);
             }
@@ -135,15 +138,17 @@ int main(){
             //make sure to close file
             file.close();
         }
-        vector<string> command = tokenize(line);
         else if(stdOutRead(command)>-1){
             //command -> child -> execvp
             //handle reading to
             int orgOut = dup(1);
-            cmdLine = new char[stdOutRead(command)];
-            for (int i = 0; i < stdOutRead(command); ++i) {
-                cmdLine[i] = (char*)commands[i].c_str();
+            int outputIndex = stdOutRead(command);
+            char* cmdLine[outputIndex + 1];
+            for (int i = 0; i < outputIndex; ++i) {
+                cmdLine[i] = (char*)command[i].c_str();
             }
+            cmdLine[outputIndex] = NULL;
+            int outFd = open(command[outputIndex + 1].c_str(), O_WRONLY | O_CREAT, 0666);
             pid_t pid;
             pid = fork();
             if(pid < 0){ //failed
@@ -152,12 +157,19 @@ int main(){
             }
             else if(pid == 0){ //child
                 //open a channel to parent
+                dup2(outFd, 1);
                 //execute the command
+                if (execvp(cmdLine[0], cmdLine) < 0) {
+                    cout << "Something went wrong" << endl;
+                }
                 //die
             }
             else{ //parent
                 //get the file reference prepped
                 //wait
+                wait(0);
+                fflush(stdout);
+                dup2(orgOut, 1);
                 //take the data from child
                 //write it to the file
             }
@@ -186,48 +198,48 @@ int main(){
             }
 
         }
-        else if(stdHasPipe(command)>-1){
+        else if (stdHasPipe(command) > -1) {
             int pipeLoc = stdHasPipe(command);
             vector<string> firstCommand;
             vector<string> secondCommand;
 
-            for(int i = 0; i<pipeLoc; i++){
+            for (int i = 0; i < pipeLoc; i++) {
                 firstCommand.push_back(command[i]);
             }
-            char* firstComPrepped[firstCommand.size()+1];
-            for(int i = 0; i<firstCommand.size(); i++){
+            char* firstComPrepped[firstCommand.size() + 1];
+            for (int i = 0; i < firstCommand.size(); i++) {
                 firstComPrepped[i] = firstCommand[i].c_str();
             }
             firstComPrepped[firstCommand.size()] = NULL;
 
-            for(int i = pipeLoc+1; i < command.size(); i++){
+            for (int i = pipeLoc + 1; i < command.size(); i++) {
                 secondCommand.push_back(command[i]);
             }
-            char* secondComPrepped[secondCommand.size()+2];
-            for(int i = 0; i<secondCommand.size(); i++){
+            char* secondComPrepped[secondCommand.size() + 2];
+            for (int i = 0; i < secondCommand.size(); i++) {
                 secondComPrepped[i] = secondCommand[i].c_str();
             }
-            secondComPrepped[secondCommand.size()+1] = NULL;
+            secondComPrepped[secondCommand.size() + 1] = NULL;
             //command -> child -> execvp
             //handle pipe
             pid_t pid;
             pid = fork();
-            if(pid < 0){ //failed
+            if (pid < 0) { //failed
                 cout << "ERROR: Cannot create a secondary process" << endl;
                 exit(2);
             }
-            else if(pid == 0){ //child
+            else if (pid == 0) { //child
                 //setup the output pipe
                 dup2(toP[1], 1);
                 close(toP[1]);
                 close(toP[0]);
                 //execute the command
-                if (execvp(fisrtComPrepped[0], firstComPrepped) < 0){
+                if (execvp(firstComPrepped[0], firstComPrepped) < 0) {
                     cout << "Error: Cannot chnage the process exe image a process" << endl;
                     exit(3);
                 }
             }
-            else{ //parent
+            else { //parent
                 //wait
                 wait(0);
                 //get the data from the first child
@@ -236,26 +248,26 @@ int main(){
                 // OR just get it when we need it from the pipe
                 pid_t pid;
                 pid = fork();
-                if(pid < 0){ //failed
+                if (pid < 0) { //failed
                     cout << "ERROR: Cannot create a secondary-secondary process" << endl;
                     exit(2);
                 }
-                else if(pid == 0){ //child
+                else if (pid == 0) { //child
                     //execute the command
                     dup2(toP[0], 0);
                     close(toP[0]);
                     close(toP[1]);
                     //get the information from the pipe
                     string childData;
-                    cin >>  childData;
+                    cin >> childData;
                     secondComPrepped[secondCommand.size()] = childData;
-                if (execvp(secondComPrepped[0], secondComPrepped) < 0){
-                    cout << "Error: Cannot chnage the process exe image a process" << endl;
-                    exit(3);
-                }
+                    if (execvp(secondComPrepped[0], secondComPrepped) < 0) {
+                        cout << "Error: Cannot chnage the process exe image a process" << endl;
+                        exit(3);
+                    }
 
                 }
-                else{ //parent
+                else { //parent
                     //wait
 
 
